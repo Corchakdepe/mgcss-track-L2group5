d9e35e2353b426e774826fa1769f9b221dee4107
ifram <iframeestudios@gmail.com>
2026-05-11 18:34:49 +0200

    test: TDD - RED failing test for non-existing getters in Solicitud

--- src/test/java/com/mgcss/unit/domain/model/SolicitudTest.java
+++ src/test/java/com/mgcss/unit/domain/model/SolicitudTest.java
@@ -5,54 +5,43 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SolicitudTest {
 
     @Test
     void cerrarSolicitudEnProcesoEstaPermitido() {
-        Solicitud solicitud = new Solicitud(1L,
-                new Cliente(1L, "", "@", TipoCliente.STANDARD),
-                "",
-                LocalDate.now(),
-                EstadoSolicitud.EN_PROCESO,
-                null);
+        Solicitud solicitud = new Solicitud(1L, new Cliente(1L, "", "@", TipoCliente.STANDARD), "", LocalDate.now(), EstadoSolicitud.EN_PROCESO, null);
         assertDoesNotThrow(solicitud::cerrar);
     }
 
     @Test
     void cerrarSolicitudNoEnProcesoEstaProhibido() {
-        Solicitud solicitud = new Solicitud(2L,
-                new Cliente(1L, "", "@", TipoCliente.STANDARD),
-                "",
-                LocalDate.now(),
-                EstadoSolicitud.ABIERTA,
-                null);
+        Solicitud solicitud = new Solicitud(2L, new Cliente(1L, "", "@", TipoCliente.STANDARD), "", LocalDate.now(), EstadoSolicitud.ABIERTA, null);
         assertThrows(IllegalStateException.class, solicitud::cerrar);
     }
 
     @Test
     void asignarTecnicoEstaPermitido() {
-        Solicitud solicitud = new Solicitud(1L,
-                new Cliente(1L, "", "@", TipoCliente.PREMIUM),
-                "",
-                LocalDate.now(),
-                EstadoSolicitud.ABIERTA,
-                null);
+        Solicitud solicitud = new Solicitud(1L, new Cliente(1L, "", "@", TipoCliente.PREMIUM), "", LocalDate.now(), EstadoSolicitud.ABIERTA, null);
         Tecnico tecnicoActivo = new Tecnico(1L, "", "", true);
         assertDoesNotThrow(() -> solicitud.asignar(tecnicoActivo));
     }
 
     @Test
     void asignarTecnicoInactivoNoEstaPermitido() {
-        Solicitud solicitud = new Solicitud(1L,
-                new Cliente(1L, "", "@", TipoCliente.PREMIUM),
-                "",
-                LocalDate.now(),
-                EstadoSolicitud.ABIERTA,
-                null);
+        Solicitud solicitud = new Solicitud(1L, new Cliente(1L, "", "@", TipoCliente.PREMIUM), "", LocalDate.now(), EstadoSolicitud.ABIERTA, null);
         Tecnico tecnicoInactivo = new Tecnico(2L, "", "", false);
         assertThrows(IllegalArgumentException.class, () -> solicitud.asignar(tecnicoInactivo));
+    }
+
+    @Test
+    void obtieneLosAtributosCorrectamente() {
+        Cliente cliente = new Cliente(1L, "", "@", TipoCliente.PREMIUM);
+        Solicitud solicitud = new Solicitud(1L, cliente, "", LocalDate.now(), EstadoSolicitud.ABIERTA, null);
+        assertEquals(1L, solicitud.getId());
+        assertEquals(cliente, solicitud.getCliente());
+        assertEquals("", solicitud.getDescripcion());
+        assertEquals(LocalDate.now(), solicitud.getFechaCreacion());
     }
 }
