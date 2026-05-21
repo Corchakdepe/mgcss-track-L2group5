6260e599fddbe4fbc8df3ffa7c1f159182fd9103
ifram <iframeestudios@gmail.com>
2026-05-21 21:39:49 +0200

    feat: api configuration class

---
+++ src/main/java/com/mgcss/infrastructure/config/DomainConfig.java
@@ -0,0 +1,21 @@
+package com.mgcss.infrastructure.config;
+
+import com.mgcss.domain.repository.ClienteRepository;
+import com.mgcss.domain.repository.SolicitudRepository;
+import com.mgcss.domain.repository.TecnicoRepository;
+import com.mgcss.service.SolicitudService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DomainConfig {
+
+    @Bean
+    public SolicitudService solicitudService(
+            SolicitudRepository solicitudRepository,
+            TecnicoRepository tecnicoRepository,
+            ClienteRepository clienteRepository) {
+        // Spring inyectará automáticamente los Adapters aquí porque implementan las interfaces
+        return new SolicitudService(solicitudRepository, tecnicoRepository, clienteRepository);
+    }
+}
