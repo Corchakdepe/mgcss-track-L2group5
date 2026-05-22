a2149e9edfaff33fca78a2ccab27ae387d44d45d
ifram <iframeestudios@gmail.com>
2026-05-22 19:29:25 +0200

    feat: implement technician creation service logic

--- src/main/java/com/mgcss/service/TecnicoService.java
+++ src/main/java/com/mgcss/service/TecnicoService.java
@@ -4,10 +4,15 @@ import com.mgcss.domain.model.Tecnico;
 import com.mgcss.domain.repository.TecnicoRepository;
 
 public class TecnicoService {
-    public TecnicoService(TecnicoRepository repoTecnico) {
+
+    private final TecnicoRepository tecnicoRepository;
+
+    public TecnicoService(TecnicoRepository tecnicoRepository) {
+        this.tecnicoRepository = tecnicoRepository;
     }
 
-    public Tecnico crearTecnico(String jesusDiaz, String mail) {
-        //TODO
+    public Tecnico crearTecnico(String nombre, String especialidad) {
+        Tecnico tecnico = new Tecnico(null, nombre, especialidad, true);
+        return tecnicoRepository.save(tecnico);
     }
 }
