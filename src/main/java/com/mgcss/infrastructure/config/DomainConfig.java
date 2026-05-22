69ef142c8f6760a7d340964f89fb799c937267cc
ifram <iframeestudios@gmail.com>
2026-05-22 16:01:31 +0200

    feat: implement client service bean

--- src/main/java/com/mgcss/infrastructure/config/DomainConfig.java
+++ src/main/java/com/mgcss/infrastructure/config/DomainConfig.java
@@ -3,6 +3,7 @@ package com.mgcss.infrastructure.config;
 import com.mgcss.domain.repository.ClienteRepository;
 import com.mgcss.domain.repository.SolicitudRepository;
 import com.mgcss.domain.repository.TecnicoRepository;
+import com.mgcss.service.ClienteService;
 import com.mgcss.service.SolicitudService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,5 +18,10 @@ public class DomainConfig {
             ClienteRepository clienteRepository) {
         // Spring inyectará automáticamente los Adapters aquí porque implementan las interfaces
         return new SolicitudService(solicitudRepository, tecnicoRepository, clienteRepository);
+    }
+
+    @Bean
+    public ClienteService clienteService(ClienteRepository clienteRepository) {
+        return new ClienteService(clienteRepository);
     }
 }
