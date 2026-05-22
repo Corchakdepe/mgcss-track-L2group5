4e82c589476a9115df1c032152e239ba66d57f0a
ifram <iframeestudios@gmail.com>
2026-05-22 19:24:58 +0200

    test: add technician creation service test

---
+++ src/main/java/com/mgcss/service/TecnicoService.java
@@ -0,0 +1,13 @@
+package com.mgcss.service;
+
+import com.mgcss.domain.model.Tecnico;
+import com.mgcss.domain.repository.TecnicoRepository;
+
+public class TecnicoService {
+    public TecnicoService(TecnicoRepository repoTecnico) {
+    }
+
+    public Tecnico crearTecnico(String jesusDiaz, String mail) {
+        //TODO
+    }
+}
---
+++ src/test/java/com/mgcss/unit/service/TecnicoServiceTest.java
@@ -0,0 +1,28 @@
+package com.mgcss.unit.service;
+
+import com.mgcss.domain.model.Tecnico;
+import com.mgcss.domain.repository.TecnicoRepository;
+import com.mgcss.service.TecnicoService;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class TecnicoServiceTest {
+
+    private static TecnicoRepository repoTecnico;
+    private static TecnicoService sut;
+
+    @Test
+    void debeCrearClienteCorrectamente() {
+        // 1. Arrange: Crear mocks y datos
+        repoTecnico = mock(TecnicoRepository.class);
+        sut = new TecnicoService(repoTecnico);
+        // Simular dependencias externas
+        when(repoTecnico.save(any(Tecnico.class))).thenAnswer(i -> i.getArgument(0));
+        // 2. Act: Ejecutar servicio
+        Tecnico tecnico = sut.crearTecnico("Jesus Diaz", "jesusdiaz@mail.com");
+        // 3. Assert: Verificar la orquestacion
+        verify(repoTecnico).save(tecnico);
+    }
+}
