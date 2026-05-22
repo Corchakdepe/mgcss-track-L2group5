79cf0fc2cc4c0a0f1c56e0801ce3549e59feec00
ifram <iframeestudios@gmail.com>
2026-05-22 19:05:39 +0200

    feat: implement technician dto's

---
+++ src/main/java/com/mgcss/api/dto/TecnicoResponseDTO.java
@@ -0,0 +1,28 @@
+package com.mgcss.api.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+@Schema(description = "Información detallada del operario técnico devuelta por el sistema")
+public class TecnicoResponseDTO extends CommonResponseDTO {
+
+    @Schema(description = "Área de especialización técnica del operario", example = "Sistemas de Redes")
+    private final String especialidad;
+
+    @Schema(description = "Disponibilidad del tecnico para resolver incidencias", example = "true")
+    private final boolean activo;
+
+    public TecnicoResponseDTO() {
+        super(0L, "");
+        especialidad = "";
+        activo = true;
+    }
+
+    public TecnicoResponseDTO(Long id, String nombre, String especialidad, boolean activo) {
+        super(id, nombre);
+        this.especialidad = especialidad;
+        this.activo = activo;
+    }
+
+}
