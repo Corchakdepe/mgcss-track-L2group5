5bdc0c7c41ba59156dbe54a9d47b401daef3e001
ifram <iframeestudios@gmail.com>
2026-05-22 15:41:00 +0200

    test: add client creation service test

---
+++ src/main/java/com/mgcss/service/ClienteService.java
@@ -0,0 +1,14 @@
+package com.mgcss.service;
+
+import com.mgcss.domain.model.Cliente;
+import com.mgcss.domain.repository.ClienteRepository;
+
+public class ClienteService {
+
+    public ClienteService(ClienteRepository repoCliente) {
+    }
+
+    public Cliente crearCliente(String cliente1, String mail) {
+        //TODO
+    }
+}
---
+++ src/test/java/com/mgcss/unit/service/ClienteServiceTest.java
@@ -0,0 +1,29 @@
+package com.mgcss.unit.service;
+
+import com.mgcss.domain.model.Cliente;
+import com.mgcss.domain.repository.ClienteRepository;
+import com.mgcss.service.ClienteService;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class ClienteServiceTest {
+
+    private static ClienteRepository repoCliente;
+    private static ClienteService sut;
+
+    @Test
+    void debeCrearClienteCorrectamente() {
+        // 1. Arrange: Crear mocks y datos
+        repoCliente = mock(ClienteRepository.class);
+        sut = new ClienteService(repoCliente);
+        // Simular dependencias externas
+        when(repoCliente.save(any(Cliente.class))).thenAnswer(i -> i.getArgument(0));
+        // 2. Act: Ejecutar servicio
+        Cliente cliente = sut.crearCliente("Cliente1", "cliente1@mail.com");
+        // 3. Assert: Verificar la orquestacion
+        verify(repoCliente).save(cliente);
+    }
+
+}
