16435fc62e6e4b2f53b6969503e44ca7781aca78
ifram <iframeestudios@gmail.com>
2026-05-22 15:25:13 +0200

    feat: implement client dto's

---
+++ src/main/java/com/mgcss/api/dto/ClienteResponseDTO.java
@@ -0,0 +1,28 @@
+package com.mgcss.api.dto;
+
+import com.mgcss.domain.model.TipoCliente;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+@Schema(description = "Información detallada del cliente devuelta por el sistema")
+public class ClienteResponseDTO extends CommonResponseDTO {
+
+    @Schema(description = "Dirección de correo electrónico del cliente", example = "juan.perez@mgcss.com")
+    private String email;
+
+    @Schema(description = "Tipo o categoría de prioridad asignada al cliente dentro del dominio")
+    private TipoCliente tipoCliente;
+
+    public ClienteResponseDTO() {
+        super(0L, "");
+        email = "";
+        tipoCliente = null;
+    }
+
+    public ClienteResponseDTO(Long id, String nombre, String email, TipoCliente tipoCliente) {
+        super(id, nombre);
+        this.email = email;
+        this.tipoCliente = tipoCliente;
+    }
+}
