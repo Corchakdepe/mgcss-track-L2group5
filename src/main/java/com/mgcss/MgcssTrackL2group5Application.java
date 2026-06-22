5189f26150971f1fc7016c6da9c724e327191810
Corchakdepe <corchakdepe@hotmail.com>
2026-06-22 16:42:16 +0200

    fix: only to force a commit pero ahora funciona 100%

--- src/main/java/com/mgcss/MgcssTrackL2group5Application.java
+++ src/main/java/com/mgcss/MgcssTrackL2group5Application.java
@@ -1,14 +1,16 @@
 package com.mgcss;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class MgcssTrackL2group5Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MgcssTrackL2group5Application.class, args);
-
+		log.debug("no hace nada es solo una prueba");
 
 	}
 
