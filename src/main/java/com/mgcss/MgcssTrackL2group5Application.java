1b22c9780e8dcf9c2a8349d82abe10902a55ea3b
Corchakdepe <corchakdepe@hotmail.com>
2026-06-22 16:27:08 +0200

    fix: only to force a commit

--- src/main/java/com/mgcss/MgcssTrackL2group5Application.java
+++ src/main/java/com/mgcss/MgcssTrackL2group5Application.java
@@ -8,6 +8,7 @@ public class MgcssTrackL2group5Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MgcssTrackL2group5Application.class, args);
+		System.out.println("No hace nada es solo para el commit");
 	}
 
 }
